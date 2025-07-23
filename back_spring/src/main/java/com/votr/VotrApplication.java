7f75b67b853a85f1409948e9de28153cf25cef17
paladin2001 <paladin2001@hanmail.net>
2025-07-24 08:54:56 +0900

    Add files via upload

---
+++ back_spring/src/main/java/com/votr/VotrApplication.java
@@ -0,0 +1,13 @@
+package com.votr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotrApplication.class, args);
+	}
+
+}
